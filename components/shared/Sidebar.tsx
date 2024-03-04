84540e4b640f5821d8dbf5f05a7183926bdaed3a
Francis Clyde Laguda Berog <122136728+clydeberog@users.noreply.github.com>
2024-03-04 15:35:27 +0800

    login enabled

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -5,6 +5,7 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -70,6 +71,9 @@ const Sidebar = () => {
           </SignedIn>
 
           <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
           </SignedOut>
         </nav>
       </div>
